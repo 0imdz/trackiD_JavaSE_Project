e3a8f9e8e4a3fac0c71887ae39a00b476eee038b
Ismael Morillas <ismaelmorillas@outlook.com>
2021-04-06 16:08:15 +0200

    Versión 1.1 con Netbeans

---
+++ src/main/java/com/mycompany/mavenproject1/panel_usuarioController.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ * FXML Controller class
+ *
+ * @author Ismael
+ */
+public class panel_usuarioController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    @FXML
+    private Button new_release;
+    
+    @FXML
+    private void switchToNewSong() throws IOException {
+        App.setRoot("registrar_cancion");
+    }
+    
+}
